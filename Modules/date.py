527119bc8b725f3b55aa494d5c76c05d5493dc5b
tufo09 <144875509+tufo09@users.noreply.github.com>
2024-03-26 01:26:58 +0100

    removed  useless letter

--- Modules/date.py
+++ Modules/date.py
@@ -14,7 +14,7 @@ def Update(module: main.Module):
     month = '0' + month if len(month) < 2 else month
     year = str(now.year)
 
-    date_str = f"{day}.{month}.{year}X"
+    date_str = f"{day}.{month}.{year}"
 
     module.full_text = date_str
 
